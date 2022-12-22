bdf4acd456a3c1e7115bd3d0d6438df03c529300
OleksandrDatskevych <Oleksandr.Datskevych@itechart-group.com>
2022-12-22 18:25:09 +0200

    Task 5: Classes, Objects and Methods

---
+++ Task5ClassesObjectsMethods/Factory.cs
@@ -0,0 +1,70 @@
+﻿using TableNS;
+
+namespace FactoryNS
+{
+    public class Factory
+    {
+        public Table[] tables = new Table[5] {
+            new Table("Epicenter"),
+            new Table("New Line", 250, 100, 35.99f),
+            new Table() { width = 100, height = 50, cost = 15.49f },
+            new Table(),
+            new Table("Table & Chair", 150, 90, 19.99f)
+        };
+
+        public void AssembleTable(Table table)
+        {
+            table.Assemble();
+        }
+
+        public int GetAmountOfTables()
+        {
+            return tables.Length;
+        }
+
+        public void ListOfTables()
+        {
+            for (int i = 0; i < tables.Length; i++)
+            {
+                Console.WriteLine($"\nTable number: {i + 1}");
+                tables[i].Print();
+            }
+        }
+
+        public void ListOfTablesByName(string name)
+        {
+            bool isFound = false;
+            for (int i = 0; i < tables.Length; i++)
+            {
+                if (tables[i].name.ToUpper() == name.ToUpper())
+                {
+                    Console.WriteLine($"Table number: {i + 1}");
+                    isFound = true;
+                    tables[i].Print();
+                }
+            }
+            if (!isFound)
+            {
+                Console.WriteLine($"Tables with name {name} were not found");
+            }
+        }
+
+        public void ListOfTablesByCost(float cost)
+        {
+            bool isFound = false;
+            for (int i = 0; i < tables.Length; i++)
+            {
+                if (tables[i].cost <= cost)
+                {
+                    Console.WriteLine($"Table number: {i + 1}");
+                    isFound = true;
+                    tables[i].Print();
+                }
+            }
+            if (!isFound)
+            {
+                Console.WriteLine($"Tables with cost below {cost} were not found");
+            }
+        }
+    }
+}
---
+++ Task5ClassesObjectsMethods/Program.cs
@@ -0,0 +1,35 @@
+﻿using FactoryNS;
+
+namespace Task5ClassesObjectsMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Factory factoryOfTables = new Factory();
+
+            int amountOfTables = factoryOfTables.GetAmountOfTables();
+            Console.WriteLine($"Amount of tables in factory {amountOfTables}");
+
+            Console.WriteLine("List of tables in factory:");
+            factoryOfTables.ListOfTables();
+
+            Console.WriteLine("\nEnter a number of table to assemble:");
+            int tableNumber = Convert.ToInt32(Console.ReadLine());
+            if ((tableNumber <= amountOfTables) & (tableNumber > 0))
+            {
+                factoryOfTables.tables[tableNumber - 1].Assemble();
+            }
+            else
+            {
+                Console.WriteLine($"There's no table with number {tableNumber}");
+            }
+
+            Console.WriteLine("\nEnter a name of table to get the list of tables by name");
+            factoryOfTables.ListOfTablesByName(Console.ReadLine());
+
+            Console.WriteLine("\nEnter your estimated budget");
+            factoryOfTables.ListOfTablesByCost(Convert.ToSingle(Console.ReadLine()));
+        }
+    }
+}
